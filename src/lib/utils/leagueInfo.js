83302d3fe9943b5170b6d16da476c95d6c0bfa0d
Mario Konja <mkonja@teamliquid.com>
2023-03-20 13:05:39 -0400

    update john team

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -132,7 +132,7 @@ export const managers = [
         preferredContact: "Carrier Pigeon", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },
     {
-        managerID: "463130879590395904", // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
+        managerID: "463133568445444096", // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
         name: "Mertic",
         location: "Tweed Heads, NSW, Australia", // (optional)
         bio: "Aussie Aussie Aussie",
