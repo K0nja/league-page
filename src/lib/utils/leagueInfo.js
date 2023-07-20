1305f69079120826dcb2acf5fa93f7db14460ffb
Mario Konja <mkonja@teamliquid.com>
2023-07-20 13:34:55 -0400

    change kosai rival

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -225,9 +225,9 @@ export const managers = [
         favoriteTeam: "det", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
         mode: "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
         rival: {
-            name: "mmccourt", // Can be anything (usually your rival's name)
-            link: 2, // manager array number within this array, or null to link back to all managers page
-            image: "/managers/mccourt.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+            name: "klimansc", // Can be anything (usually your rival's name)
+            link: 5, // manager array number within this array, or null to link back to all managers page
+            image: "/managers/scott.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
         },
         favoritePlayer: 8205, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
         valuePosition: "RB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
