0d93ee7bb048cb645912ab1c7453e62ab7e8d366
Mario Konja <mkonja@teamliquid.com>
2023-07-28 13:14:44 -0400

    update scott record vs fred

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -114,7 +114,7 @@ export const managers = [
         managerID: "463130879590395904", // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
         name: "klimansc",
         location: "Denver, CO, USA", // (optional)
-        bio: "I’m 67-22-16 and counting against Fred in iPhone darts.",
+        bio: "I’m 72-22-17 and counting against Fred in iPhone darts.",
         photo: "/managers/scott.jpg", // square ratio recommended (no larger than 500x500)
         fantasyStart: 2001, // (optional) when did the manager start playing fantasy football
         favoriteTeam: "was", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
