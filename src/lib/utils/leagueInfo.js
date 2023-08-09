472da539822297ddd839eb6a25c760032544a179
Mario Konja <mkonja@teamliquid.com>
2023-08-09 14:12:44 -0400

    fix kosai user name

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -217,7 +217,7 @@ export const managers = [
     },
     {
         managerID: "463399096221167616", // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
-        name: "kzaya15",
+        name: "kzaya5",
         location: "South Lyon, MI, USA", // (optional)
         bio: "This is fucking stupid",
         photo: "/managers/kosai.jpg", // square ratio recommended (no larger than 500x500)
