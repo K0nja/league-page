3e7c7f8de3e6eec072970c08d5b8812d98b281c9
Mario Konja <mkonja@teamliquid.com>
2023-08-25 09:44:18 -0400

    fix zack fav player 2

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -100,7 +100,7 @@ export const managers = [
         bio:
             "Full blown cat daddy. Considering a transition to only having players from cat teams - Lions, Jags, Panthers, Bengals. Send trades.",
         photo: "/managers/zack.jpg", // square ratio recommended (no larger than 500x500)
-        fantasyStart: 684, // (optional) when did the manager start playing fantasy football
+        fantasyStart: 2008, // (optional) when did the manager start playing fantasy football
         favoriteTeam: "det", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
         mode: "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
         rival: {
@@ -108,7 +108,7 @@ export const managers = [
             link: null, // manager array number within this array, or null to link back to all managers page
             image: "/managers/everyone.png", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
         },
-        favoritePlayer: 2306, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
+        favoritePlayer: 684, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
         valuePosition: "QB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
         rookieOrVets: "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
         philosophy:
