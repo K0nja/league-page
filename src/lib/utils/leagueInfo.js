f69708422653e61e487f093a907fb833e196a04b
Mario Konja <mkonja@teamliquid.com>
2023-08-25 09:41:08 -0400

    update zack fav player

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -100,7 +100,7 @@ export const managers = [
         bio:
             "Full blown cat daddy. Considering a transition to only having players from cat teams - Lions, Jags, Panthers, Bengals. Send trades.",
         photo: "/managers/zack.jpg", // square ratio recommended (no larger than 500x500)
-        fantasyStart: 2008, // (optional) when did the manager start playing fantasy football
+        fantasyStart: 684, // (optional) when did the manager start playing fantasy football
         favoriteTeam: "det", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
         mode: "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
         rival: {
