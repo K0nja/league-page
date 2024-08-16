2bef76377b661b6897aa86e0c7bac74b0b08885b
K0nja <mariok1992@gmail.com>
2024-08-16 08:35:55 -0400

    update fred info 2024

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -212,22 +212,22 @@ export const managers = [
         name: "fshatara2",
         location: "Detroit, MI, USA", // (optional)
         bio:
-            "Self proclaimed and unanimously undisputed favorite child. Lover of all things narcotics and stimulants. Did I mention I'm platinum status with Delta?",
+            "Former champion, pulled off the biggest trade fleece of all time, but my most important achievement — sole owner of 1/10/1994. I like my RBs like I like my bourbon; aged 30+ years and overpriced.",
         photo: "/managers/fred.jpg", // square ratio recommended (no larger than 500x500)
         fantasyStart: 2012, // (optional) when did the manager start playing fantasy football
         favoriteTeam: "buf", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-        mode: "Dynasty", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
+        mode: "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
         rival: {
-            name: "Princess Peach", // Can be anything (usually your rival's name)
-            link: 2, // manager array number within this array, or null to link back to all managers page
-            image: "/managers/mccourt.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
+            name: "Coward", // Can be anything (usually your rival's name)
+            link: 8, // manager array number within this array, or null to link back to all managers page
+            image: "/managers/fred.jpg", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
         },
         favoritePlayer: 1525, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
         valuePosition: "QB", // (optional) Favorite position (QB, WR, RB, TE, etc.)
         rookieOrVets: "Vets", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
-        philosophy: "Josh Allen until I die (or whenever Micah retires).",
-        tradingScale: 7, // 1 - 10
-        preferredContact: "Text", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
+        philosophy: "Josh Allen",
+        tradingScale: 10, // 1 - 10
+        preferredContact: "Email", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },
     {
         managerID: "463399096221167616", // the user's manager ID, go to https://api.sleeper.app/v1/league/<your_league_id>/users to find user IDs (you can use older leagueIDs to find user IDs for managers that are no longer in the league)
