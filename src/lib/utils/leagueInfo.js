a0ac0a935d518b9834e226fff5456decfe2a5bb1
K0nja <mariok1992@gmail.com>
2024-08-22 09:40:54 -0400

    update mario team 2024

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -33,7 +33,7 @@ export const managers = [
             "I went too far in a rebuild and got very lucky, BUT WE TAKE THOSE.  I have donated dues for 5 straight years.  Here's to at least 5 more 🍻🥂",
         photo: "/managers/konja.jpg", // square ratio recommended (no larger than 500x500)
         fantasyStart: 2014, // (optional) when did the manager start playing fantasy football
-        favoriteTeam: "cle", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+        favoriteTeam: "ari", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
         mode: "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
         rival: {
             name: "Myself", // Can be anything (usually your rival's name)
