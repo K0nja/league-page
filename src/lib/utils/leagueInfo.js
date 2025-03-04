ae569d9bde7e913ebd620ca26b2e17296983f978
K0nja <mariok1992@gmail.com>
2025-03-03 19:05:09 -0500

    update league year to 2025

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1052162650571923456"; // your league ID
+export const leagueID = "1202123010564161536"; // your league ID
 export const leagueName = "League of Haiwans"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
